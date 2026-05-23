9d0de325ea0b3a5e84f0db817357d2298e3e5b18
qldbaseballscorers <danicatrotter@gmail.com>
2026-05-23 18:46:35 +1000

    Add missing Vite entry files and config

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Tracker from './pages/tracker.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Tracker />
+  </React.StrictMode>,
+)
